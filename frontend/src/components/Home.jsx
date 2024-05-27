8ea9a536c1deb134720f74af069f8cde6e77a4e5
yudi <kawasaki89.team@gmail.com>
2024-05-27 18:21:03 +0700

    perbaiki halaman home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
   return (
     <Container className="mt-5 text-center">
-      <h1>Welcome to Universitas Nasional</h1>
+      <h1>Welcome to Universitas Komedi</h1>
       <p className="mt-3">This is the home page of the student attendance system.</p>
       <Link to="/admin-login">
         <Button variant="primary" className="mt-3">Login as Admin/Dosen</Button>
